79fb471404231f227f15ddafd17e0e4283b7d5c6
Omar D <omar.drwch@gmail.com>
2018-07-16 14:52:49 +0200

    updating setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='mfanalysis',
       version='0.12dev',
       description='Implements wavelet based multifractal analysis of 1d signals.',
-      url='https://gitlab.inria.fr/odarwich/omar_darwiche_domingues',
+      url='https://github.com/neurospin/mfanalysis',
       author='Omar Darwiche Domingues',
       author_email='',
       license='',
