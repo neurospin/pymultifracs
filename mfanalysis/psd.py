d8241a08e9cd44bff5e4663e60eb0eb5f32caa6f
MerlinDumeur <merlin@dumeur.net>
2019-11-18 16:31:06 +0100

    Improved psd plotting and added doc

--- mfanalysis/psd.py
+++ mfanalysis/psd.py
@@ -4,26 +4,60 @@ import matplotlib.pyplot as plt
 
 from .mfa import MFA
 
-def plot_psd(signal, fs):
+def plot_psd(signal, fs, n_fft=4096, segment_size=None, n_moments=2):
+    """
+    Plot the superposition of Fourier-based Welch estimation and Wavelet-based estimation of PSD on a log-log graphic.
+    Based on the `wavelet_fourier_spectrum` function of the MATLAB mf_bs_toolbox-v0.2
+    
+    Parameters
+    ----------
+    signal: 1D-array_like
+        Time series of sampled values
 
-    overlapingratio = 4
-    window_size = np.power(2, np.fix(np.log2(len(signal))) - overlapingratio)
+    fs: float 
+        Sampling frequency of the signal
+
+    n_fft: int, optional
+        Length of the FFT desired.
+        If `segment_size` is greater, ``n_fft = segment_size``.
+    
+    segment_size: int | None
+        Length of Welch segments.
+        Defaults to None, which sets it equal to `n_fft`
+
+    overlapingratio: int
+        Defined as ceil(log2(len(signal))) / log2(segment_size)
+
+    n_moments: int
+        Number of vanishing moments of the Daubechies wavelet used in theWavelet decomposition.
+
+    """
+
+    if segment_size is None:
+        segment_size = n_fft
+
+    if n_fft < segment_size:
+        n_fft = segment_size
+
+    # Fourier / Welch part
 
     _,pds_Fourier = welch(signal,
                     window='hamming',
-                    nperseg=window_size,
-                    noverlap=window_size/2,
-                    nfft=4096,
+                    nperseg=segment_size,
+                    noverlap=segment_size/2,
+                    nfft=n_fft,
                     detrend=False,
                     return_onesided=True,
                     scaling='density',
                     average='mean',
                     fs = 2*np.pi)
 
-    f_Fourier = fs * np.linspace(0,0.5, window_size / 2 + 1)
-    f_Fourier, pds_Fourier = np.log2(f_Fourier[1:]), np.log2(pds_Fourier[1:]) + 2
+    f_Fourier = fs * np.linspace(0,0.5, segment_size / 2 + 1)
+    f_Fourier, pds_Fourier = np.log2(f_Fourier[1:]), np.log2(pds_Fourier[1:]) + 2  # +2 is to compensate for negative frequencies
 
-    mfa = MFA(wt_name=f'db2',
+    # Wavelet part
+
+    mfa = MFA(wt_name=f'db{n_moments}',
                  formalism='wcmf',
                  j1=1,j2=13,
                  n_cumul=2,
@@ -44,15 +78,16 @@ def plot_psd(signal, fs):
     
     j_wavelet = np.arange(len(pds_wavelet))+1
     f_wavelet = (3/4 * fs) / (np.power(2, j_wavelet))
-    
     f_wavelet, pds_wavelet = np.log2(f_wavelet), np.log2(pds_wavelet)
+
+    # Plotting
 
     plt.plot(f_Fourier, pds_Fourier)
     plt.plot(f_wavelet, pds_wavelet)
     plt.legend(['Fourier', 'Wavelet'])
     plt.xlabel('log_2 f')
     plt.ylabel('log_2 S(f)')
-    plt.title('Power Spectral Density log-log plot')
+    plt.title('Power Spectral Density')
     plt.show()
 
     plt.show()
