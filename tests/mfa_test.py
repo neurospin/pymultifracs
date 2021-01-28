0291cabb62fbf1e89905c5b230ee50a6087a8446
Merlin Dumeur <merlin@dumeur.net>
2021-01-28 17:42:59 +0100

    Fixed doc build, added code coverage

--- tests/mfa_test.py
+++ tests/mfa_test.py
@@ -4,6 +4,7 @@ import json
 from pymultifracs.wavelet import wavelet_analysis
 from pymultifracs.estimation import estimate_hmin
 from pymultifracs.mfa import mf_analysis_full
+from pymultifracs.utils import build_q_log
 
 
 def test_mfa_fbm(fbm_file):
@@ -23,8 +24,10 @@ def test_mfa_fbm(fbm_file):
         hmin = hmin.min()
         gamint = 0.0 if hmin >= 0 else -hmin + 0.1
 
+        q = np.array([-2, -1, 0, 1, 2])
+
         dwt, lwt = mf_analysis_full(X, j1=3, j2=j2_eff, gamint=gamint,
-                                    p_exp=np.inf, n_cumul=3)
+                                    p_exp=np.inf, n_cumul=3, q=q)
         if config_list[i]['H'] != 0.01:
             assert abs(dwt.structure.H.mean() - config_list[i]['H']) < 0.1,\
                 print(f'{dwt.structure.H.mean()=}, {config_list[i]["H"]=}, '
@@ -32,7 +35,7 @@ def test_mfa_fbm(fbm_file):
         assert abs(lwt.cumulants.log_cumulants[1, :].mean()) < 0.01
 
         _, lwt = mf_analysis_full(X, j1=3, j2=j2_eff, gamint=gamint, p_exp=2,
-                                  n_cumul=3)
+                                  n_cumul=3, q=q)
         assert abs(lwt.cumulants.log_cumulants[1, :].mean()) < 0.01
 
 
@@ -56,13 +59,15 @@ def test_mfa_mrw(mrw_file):
         hmin = hmin.min()
         gamint = 0.0 if hmin >= 0 else -hmin + 0.1
 
+        q = np.array([-2, -1, 0, 1, 2])
+
         dwt, lwt = mf_analysis_full(X, j1=3, j2=j2_eff, gamint=gamint,
-                                    p_exp=np.inf, n_cumul=3)
+                                    p_exp=np.inf, n_cumul=3, q=q)
         assert abs(dwt.structure.H.mean() - config_list[i]['H']) < 0.1
         assert abs(lwt.cumulants.log_cumulants[1, :].mean()
                    + (config_list[i]['lam'] ** 2)) < 0.025
 
         mf_analysis_full(X, j1=3, j2=j2_eff, gamint=gamint, p_exp=2,
-                         n_cumul=3)
+                         n_cumul=3, q=q)
         assert abs(lwt.cumulants.log_cumulants[1, :].mean()
                    + (config_list[i]['lam'] ** 2)) < 0.025
