6ba11860068fb6e023e9e0e0707f2e8179246e63
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2021-02-09 09:17:35 +0100

    enh setup + readme

--- README.rst
+++ README.rst
@@ -67,7 +67,7 @@ Using pip
 
 .. code:: shell
 
-    pip install git+https://github.com/neurospin/pymultifracs.git@master
+    pip install -U https://github.com/neurospin/pymultifracs/archive/master.zip
 
 
 Cloning the whole repository (including examples)
--- setup.py
+++ setup.py
@@ -1,16 +1,51 @@
+import os.path as op
 from setuptools import setup
 
+with open('README.rst', 'r') as fid:
+    long_description = fid.read()
+
+# get the version (don't import mne here, so dependencies are not needed)
+version = None
+with open(op.join('pymultifracs', '_version.py'), 'r') as fid:
+    for line in (line.strip() for line in fid):
+        if line.startswith('__version__'):
+            version = line.split('=')[1].strip().strip('\'')
+            break
+if version is None:
+    raise RuntimeError('Could not determine version')
+
+
 setup(name='pymultifracs',
-      version='0.1',
+      version=version,
       description='Implements wavelet based fractal and multifractal analysis '
                   'of 1d signals.',
       url='https://github.com/neurospin/pymultifracs',
       author='Omar Darwiche Domingues, Merlin Dumeur',
       author_email='',
-      license='',
+      license='MIT',
       packages=['pymultifracs'],
       install_requires=[
           'numpy', 'scipy', 'scikit-learn', 'pywavelets', 'seaborn'
       ],
       zip_safe=False,
-      python_requires='>=3.7')
+      python_requires='>=3.7',
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
+      classifiers=['Intended Audience :: Science/Research',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development',
+                   'Topic :: Scientific/Engineering',
+                   'Operating System :: Microsoft :: Windows',
+                   'Operating System :: POSIX',
+                   'Operating System :: Unix',
+                   'Operating System :: MacOS',
+                   'Programming Language :: Python :: 3'],
+      project_urls={
+        'Documentation': 'https://neurospin.github.io/pymultifracs/',
+        'Source': 'https://github.com/neurospin/pymultifracs',
+        'Tracker': 'https://github.com/neurospin/pymultifracs/issues/',
+      },
+      platforms='any',
+      )
