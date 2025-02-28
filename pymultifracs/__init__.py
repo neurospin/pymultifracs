1989e21113d10b39e3a76c8791acbefced898818
Merlin Dumeur <merlin@dumeur.net>
2025-02-28 18:16:42 +0100

    Benchmark code improvement and documentation

--- pymultifracs/__init__.py
+++ pymultifracs/__init__.py
@@ -4,3 +4,4 @@ from ._version import __version__  # noqa: F401
 
 from .mf_analysis import mfa  # noqa: F401
 from .wavelet import wavelet_analysis  # noqa: F401
+from .robust.benchmark import Benchmark  # noqa: F401
--- pymultifracs/robust/benchmark.py
+++ pymultifracs/robust/benchmark.py
@@ -5,6 +5,7 @@ Authors: Merlin Dumeur <merlin@dumeur.net>
 from pathlib import Path
 from dataclasses import dataclass, field
 from typing import Any, Callable
+from functools import reduce
 
 import numpy as np
 import pandas as pd
@@ -17,38 +18,47 @@ from .robust import get_outliers
 from ..simul.noisy import gen_noisy
 
 
-def get_grid(param_grid):
+def get_grid(param_grid, func_grid):
     """
-    Creates a grid from a parameter dictionary.
+    Creates a grid from a parameter and a function dictionary.
     """
 
-    series = [
-        pd.DataFrame({name: signal_param})
-        for name, signal_param in param_grid.items()]
+    param_dfs = {
+        fun: [pd.DataFrame({name: signal_param})
+              for name, signal_param in param_grid[fun].items()]
+        for fun in func_grid
+    }
 
-    out = series[0]
+    merge = lambda left, right: pd.merge(left, right, how='cross')
 
-    for s in series[1:]:
-        out = out.merge(s, how='cross')
+    # replace empty parameter dataframes
+    for fun in param_dfs:
+        if len(param_dfs[fun]) == 0:
+            param_dfs[fun] = [pd.DataFrame(index=[fun])]
 
-    return out
+    param_dfs = {
+        fun: reduce(merge, param_dfs[fun])
+        for fun in param_dfs
+    }
+
+    return pd.concat(param_dfs, names=['model', 'parameters'])
 
 
-def get_fname(param):
+def get_fname(name, param, folder='.'):
     """
     Create filename from parameters.
     """
 
-    fname = Path('.')
+    fname = Path(folder)
 
-    for param in param.values():
+    for param in [*param.values()]:
 
         if isinstance(param, float):
             fname /= f'{param:.2f}'
         else:
             fname /= str(param)
 
-    fname /= 'signal'
+    fname /= name
 
     return fname.with_suffix('.npy')
 
@@ -56,17 +66,46 @@ def get_fname(param):
 @dataclass
 class Benchmark:
     """
-    Compute and plot benchmarks.
+    Compute and plot benchmarks, varying the models and analysis parameters.
+
+    Arguments
+    ---------
+    signal_gen_grid : dict[str, Callable]
+        Dictionary associating a name to a function that samples time series.
+    signal_param_grid : dict[str, ndarray of Any] | \
+            dict[str, dict[str, ndarray of Any]]
+        Dictionary associating to each signal generating function the
+        dictionary associating to each signal parameter the array of the
+        values that the parameter will take. May be empty.
+    estimation_grid : dict[str, Callable]
+        Dictionary associating to an estimation method's name its callable
+        function.
+    estimation_param_grid : dict[str, dict[str, ndarray of Any]]
+        Dictionary associating to each estimation method the dictionary of
+        estimation parameters. May be empty.
+    folder : Path
+        Path to the folder which will contain the output files.
+
+    Attributes
+    ----------
+    results : DataFrame
+        Dataframe collecting the outcomes of the estimation on the generated
+        signals.
+
     """
-    signal_param_grid: dict[str, np.ndarray]
+    signal_gen_grid: dict[str, Callable]
+    signal_param_grid: dict[str, dict[str, np.ndarray]]
     # noise_param_grid: dict[str, np.ndarray]
-    signal_func: Callable
     # noise_gen_func: Callable
     estimation_grid: dict[str, Callable]
-    WT_params: dict[str, Any]
+    estimation_param_grid: dict[str, dict[str, Callable]]
+    # WT_params: dict[str, Any]
     folder: Path = Path('.')
     # parameters_df: pd.DataFrame = field(init=False, default=None, repr=False)
     results: pd.DataFrame = field(init=False, repr=False)
+
+    def __post_init__(self):
+        self._load_df()
 
     def get_df_fnames(self):
         """
@@ -74,183 +113,142 @@ class Benchmark:
         """
         return Path(self.folder / 'results.pkl')
 
-    def generate_grids(self):
+    def _generate_grids(self):
         """
-        Generate parameter grid.
+        Generate signal generation and estimation parameter grids.
         """
 
-        signal_grid = get_grid(self.signal_param_grid)
+        signal_grid = get_grid(self.signal_param_grid, self.signal_gen_grid)
+
+        estimation_grid = get_grid(
+            self.estimation_param_grid, self.estimation_grid)
         # noise_grid = get_grid(self.noise_param_grid)
 
-        return signal_grid  # , noise_grid
+        return signal_grid, estimation_grid  # , noise_grid
 
-    def load_df(self):
+    def _generate_load_signal(self, name, params, save_load=False):
+
+        fname = get_fname(name, params, self.folder)
+
+        if save_load and fname.exists():
+            return np.load(fname)
+
+        out = self.signal_gen_grid[name](**params)
+
+        if save_load:
+            np.save(fname, out)
+
+        return out
+
+    def _load_df(self):
         """
         Load results dataframe.
+
+        Currently loads systematically the latest results dataframe
+        (disregards changes in parameters)
         """
 
         results_fname = self.get_df_fnames()
         # self.parameters_df = self.generate_grids()
 
         if results_fname.exists():
+            print('Latest results dataframe found and loaded')
             self.results = pd.read_pickle(results_fname)
 
-    def compute_benchmark(self, n_jobs=1, save=False):
+    def compute_benchmark(self, n_jobs=1, save_load_signals=False, save=False):
         """
         Compute the benchmark.
+
+        Parameters
+        ----------
+        n_jobs : int
+            Number of jobs that joblib will start in parallel. Each job handles
+            a different signal configuration.
+        save_load_signals : bool
+            Whether to save the signals, and load them if found, to speed up
+            computation
+        save : bool
+            Whether the save the final results dataframe to a pickled file.
         """
 
         results = {}
 
-        signal_grid = get_grid(self.signal_param_grid)
-        signal_names = signal_grid.columns
-        # print(signal_names)
-        # signals, signal_names = self.load_generate_signals()
+        signal_param_grid, estimation_param_grid = self._generate_grids()
 
-        def estimate_mf(signal, signal_params):
+        # print(estimation_param_grid)
+
+        # N_signals = sum(df.shape[0] for df in signal_param_grid.values())
+
+        # def signal_iterator():
+        #     for fun in signal_param_grid:
+        #         for _, param_set in signal_param_grid[fun].iterrows():
+        #             yield fun, param_set
+
+        # signals_it = signal_iterator()
+
+        def estimate_mf(signal, model, signal_params):
+
+            # out = None
+
             res = []
-            WT = wavelet_analysis(signal, **self.WT_params)
-            for method, est_fun in self.estimation_grid.items():
-                res.append((method, est_fun(WT)))
 
-            return res, signal_params
+            for (method, _), est_params in estimation_param_grid.iterrows():
 
-        print(signal_grid)
+                est_fun = self.estimation_grid[method]
+
+                # only feed the parameters expected by the estimation function
+                param_restriction = {
+                    k: v for k, v in est_params.to_dict().items()
+                    if k in self.estimation_param_grid[method]
+                }
+
+                df = pd.DataFrame.from_dict(
+                    est_fun(signal, **param_restriction))
+                df = df.assign(**est_params.to_dict(), method=method)
+                df.index.name = 'k'
+
+                res.append(df)
+
+            res = pd.concat(res)
+
+            res = res.assign(model=model, **signal_params)
+            res = res.reset_index().set_index(
+                ['model', *signal_params, 'method',
+                 *estimation_param_grid.columns, 'k'])
+
+            return res
+
+        # Wrapping signal generation to not feed the wrong parameters
+        def signal_gen_wrap(model, signal_params):
+
+            signal_params = signal_params.to_dict()
+
+            restricted_params = {
+                k: v for k, v in signal_params.items()
+                if k in self.signal_param_grid[model]
+            }
+
+            return self.signal_gen_grid[model](**restricted_params)
+
         results = Parallel(n_jobs=n_jobs)(
-            delayed(estimate_mf)(*s)
-            for s in tqdm(self.signal_func(signal_grid),
-                          total=signal_grid.shape[0]))
+            delayed(estimate_mf)(
+                signal_gen_wrap(index, data), index, data.to_dict())
+            for (index, _), data in tqdm(
+                signal_param_grid.iterrows(),
+                total=signal_param_grid.shape[0])
+        )
 
-        results = {
-            (method, *signal_params.values()): [estimate]
-            for res_list, signal_params in results
-            for method, estimate in res_list
-        }
+        self.results = pd.concat(results).sort_index()
 
-        self.results = pd.DataFrame.from_dict(results).transpose()
+        if save:
 
-        self.results.index.names = ['method', *signal_names]
-        self.results.columns = ['cumulants']
+            results_fname = self.get_df_fnames()
+            results_fname.parent.mkdir(parents=True, exist_ok=True)
 
-        results_fname = self.get_df_fnames()
-        results_fname.parent.mkdir(parents=True, exist_ok=True)
+            self.results.to_pickle(results_fname)
 
-        self.results.to_pickle(results_fname)
-
-    def plot(self):
+    def _plot(self):
         """
         Plotting function. Empty for now.
         """
         return
-
-
-def estimate(gen_func, robust_cm=False, bootstrap_weight=False,
-             outlier_detect=False, alpha=1, generalized=False,
-             gen_func_kwargs=None, robust_kwargs=None):
-    """
-    Automated estimation.
-    """
-
-    if gen_func_kwargs is None:
-        gen_func_kwargs = {}
-
-    if robust_kwargs is None:
-        robust_kwargs = {}
-
-    p_exp = 2
-    noisy_X = gen_func(**gen_func_kwargs)
-
-    coefs = wavelet_analysis(
-        noisy_X.reshape(noisy_X.shape[0], -1), wt_name='db6')
-
-    coefs = coefs.integrate(gamint=1)
-
-    R = 1
-    weighted = None
-
-    if bootstrap_weight:
-        R = 25
-        weighted = 'bootstrap'
-
-    if outlier_detect:
-
-        _, idx_reject = get_outliers(
-            coefs, [(6, 11)], robust_cm=robust_cm,
-            generalized=generalized,
-            hilbert_weighted=False, **robust_kwargs)
-
-        lwt = mfa(coefs.get_leaders(p_exp), scaling_ranges=[(6, 11)],
-                  n_cumul=4, idx_reject=idx_reject, R=R, weighted=None)
-
-    else:
-
-        try:
-            lwt = mfa(
-                coefs.get_leaders(p_exp), scaling_ranges=[(6, 11)],
-                robust=robust_cm, R=R, weighted=weighted, n_cumul=4,
-                robust_kwargs=robust_kwargs)
-        except ValueError:
-            return None
-
-    return lwt
-
-
-def gen_estimate(N, gen_func, SNRgrid, covgrid, align_scale, n_jobs=10,
-                 n_rep=1, robust_cm=False, bootstrap_weight=False,
-                 outlier_detect=False, lambd=None, generalized=False,
-                 robust_kwargs=None):
-    """
-    Generate signals and estimate multifractal properties.
-    """
-
-    df_list = []
-
-    for _ in range(n_rep):
-
-        signal, noise = gen_func(N=N, lambd=lambd)
-
-        SNRlist = np.array_split(SNRgrid, n_jobs)
-        gen_func_kwargs = dict(
-            signal=signal, noise=noise, coverage=covgrid,
-            align_scale=align_scale,
-        )
-
-        cms = Parallel(n_jobs=n_jobs)(
-            delayed(estimate)(
-                gen_noisy,
-                robust_cm=robust_cm, bootstrap_weight=bootstrap_weight,
-                outlier_detect=outlier_detect, alpha=1.1,
-                generalized=generalized,
-                gen_func_kwargs={**gen_func_kwargs, 'SNR': snr},
-                robust_kwargs=robust_kwargs)
-            for snr in SNRlist)
-
-        c1_dfs = [
-            pd.DataFrame(
-                cms[i].cumulants.c1.squeeze().reshape(-1, covgrid.shape[0]),
-                index=SNR, columns=covgrid)
-            for i, SNR in enumerate(SNRlist) if cms[i] is not None]
-        c2_dfs = [
-            pd.DataFrame(
-                cms[i].cumulants.c2.squeeze().reshape(-1, covgrid.shape[0]),
-                index=SNR, columns=covgrid)
-            for i, SNR in enumerate(SNRlist) if cms[i] is not None]
-        # c3_dfs = [
-        #     pd.DataFrame(cms[i].c3.squeeze().reshape(-1, covgrid.shape[0]),
-        #                 index=SNR, columns=covgrid)
-        #     for i, SNR in enumerate(SNRlist)]
-
-        c1_df = pd.concat(c1_dfs).rename_axis(
-            columns='coverage', index=['SNR'])
-        c2_df = pd.concat(c2_dfs).rename_axis(
-            columns='coverage', index=['SNR'])
-        # c3_df = pd.concat(c3_dfs).rename_axis(
-        #     columns='coverage', index=['SNR'])
-
-        df = pd.concat([c1_df, c2_df], keys=['c1', 'c2'], axis=1,
-                       names=['cumulant', 'coverage'])
-
-        df_list.append(df)
-
-    return pd.concat(df_list, axis=0, keys=np.arange(n_rep),
-                     names=['rep', 'SNR'])
